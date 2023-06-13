bc781179f60328228f01af93c911b2e9da9cfad4
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-06-13 12:46:17 -0300

    Args
    Entendendo sobre empacotamento com args

--- return.py
+++ return.py
@@ -13,3 +13,9 @@ soma2 = soma(3, 3)
 print(soma1)
 print(soma2)
 print(soma(11, 55))
+#-------------------------------------#
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print(outra_soma)
+
+print(sum(numeros))
