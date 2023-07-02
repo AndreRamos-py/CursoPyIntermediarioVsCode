7d1ac19c733bb9a2cd49bc58f4dfaa1880945ceb
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-07-02 14:10:41 -0300

    Introdução a set

---
+++ set.py
@@ -0,0 +1,22 @@
+
+
+
+s1 = set()  # vazio
+s1 = {'Luiz', 1, 2, 3}  # com dados
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Não aceitam valores mutáveis;
+# - Seus valores serão sempre únicos;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
