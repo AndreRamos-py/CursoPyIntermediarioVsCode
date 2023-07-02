a43ef896871790a1e60049fff832158cfa2c70db
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-07-02 00:35:49 -0300

    Introdução a dicionarios
    Dict

---
+++ dicts.py
@@ -0,0 +1,21 @@
+
+
+
+pessoa = {
+    'nome': 'André',
+    'sobrenome': 'Ramos',
+    'idade': 20,
+    'altura': 1.8,
+    'endereços': [
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'outra rua', 'número': 321},
+    ],
+}
+# print(pessoa, type(pessoa))
+print(pessoa['nome'])
+print(pessoa['sobrenome'])
+
+print()
+
+for chave in pessoa:
+    print(chave, pessoa[chave])
