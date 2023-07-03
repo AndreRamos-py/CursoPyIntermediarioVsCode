4edcee22c66e3ae026fbf9b293e33336f4f42ef3
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-07-03 12:02:08 -0300

    Introdução a funções lambda
    Aprendendo sobre funções lambda e sua utilidade.

---
+++ lambda.py
@@ -0,0 +1,60 @@
+
+
+
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
+
+
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
+
+#---------------------------------------------------------#
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+# def soma(x, y):
+#     return x + y
+
+
+# def cria_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero * multiplicador
+#     return multiplica
+
+
+# duplica = cria_multiplicador(2)
+duplica = executa(
+    lambda m: lambda n: n * m,
+    2
+)
+print(duplica(2))
+
+print(
+    executa(
+        lambda x, y: x + y,
+        2, 3
+    ),
+)
+
+print(
+    executa(
+        lambda *args: sum(args),
+        1, 2, 3, 4, 5, 6, 7
+    )
+)
